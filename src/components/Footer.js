49aaa0465d580c203ac15a7e04a8ef72c3d1fb87
ammarayaz <ammar.ayaz@gmail.com>
2023-01-23 11:17:11 +0530

    first commint

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import { pageLinks, socialIcons } from "../data";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+            return (
+                <li key={link.id}>
+                    <a href={link.href} className="footer-link">{link.text}</a>
+                </li>
+            );
+        } )}
+       
+        <li>
+          <a href="#featured" className="footer-link">featured</a>
+        </li>
+      </ul>
+      <ul className="footer-icons">
+        {socialIcons.map((link) => {
+            const {id, href, icon} = link;
+            return (
+                <li key={id}>
+                <a href={href} target="_blank" rel="noreferrer" className="footer-icon">
+                    <i className={icon}></i>
+                </a>
+                </li>
+            );
+        })};
+        
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
